ea5f4bc646758a294a339a446ebf9dfcb40339d7
Inna <tasun.inna@gmail.com>
2017-10-27 01:11:20 +0300

    add

---
+++ 1.Введение/3/script.js
@@ -0,0 +1,9 @@
+/*
+Создайте JavaScript файл со следующим содержимым:
+let arr = ["World", "Tom", "Bob"];
+arr.forEach((value) => console.log(`Hello ${value}!`));
+Выполните данный код из файла при помощи Node.js.
+*/
+
+let arr = ["World", "Tom", "Bob"];
+arr.forEach((value) => console.log(`Hello ${value}!`));
---
+++ 2.Модули/rectangle.js
@@ -0,0 +1,2 @@
+module.exports.perimeter = (x, y) => (x + y)*2;
+module.exports.area = (x, y) => x * y;
